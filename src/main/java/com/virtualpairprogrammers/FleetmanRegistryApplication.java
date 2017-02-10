943384e6c89c3a7e8fabf38eb8af4bd314e2a71b
VPPCourseUser <contact@capabilitytm.com>
2017-02-10 17:23:34 +0000

    Update FleetmanRegistryApplication.java

--- src/main/java/com/virtualpairprogrammers/FleetmanRegistryApplication.java
+++ src/main/java/com/virtualpairprogrammers/FleetmanRegistryApplication.java
@@ -2,49 +2,13 @@ package com.virtualpairprogrammers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.commons.util.InetUtils;
-import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.Scheduled;
-
-import com.netflix.appinfo.AmazonInfo;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class FleetmanRegistryApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FleetmanRegistryApplication.class, args);
 	}
-
-	// <<Added for New Course
-	// Problem - this data changes after the EIP has been allocated, it seems 
-	// this stops correct functioning. 
-	@Bean
-	public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils utils) {
-		final EurekaInstanceConfigBean instance = new EurekaInstanceConfigBean(utils);
-//		{
-//			@Scheduled(initialDelay = 30000L, fixedRate = 30000L)
-//			public void refreshInfo() {
-//				AmazonInfo newInfo = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
-//				if (!this.getDataCenterInfo().equals(newInfo)) {
-//					((AmazonInfo) this.getDataCenterInfo()).setMetadata(newInfo.getMetadata());
-//					this.setHostname(newInfo.get(AmazonInfo.MetaDataKey.publicHostname));
-//					this.setIpAddress(newInfo.get(AmazonInfo.MetaDataKey.publicIpv4));
-//					this.setDataCenterInfo(newInfo);
-//					this.setNonSecurePort(8010);
-//				}
-//			}         
-//		};
-		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
-		instance.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
-		instance.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
-		instance.setDataCenterInfo(info);
-		instance.setNonSecurePort(8010);
-
-		return instance;
-	}
-
-
 }
