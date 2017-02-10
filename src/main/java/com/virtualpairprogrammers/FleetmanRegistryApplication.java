a4278014d099147ba85e71ef74d5081e4d42dc60
VPPCourseUser <contact@capabilitytm.com>
2017-02-10 16:51:53 +0000

    Update FleetmanRegistryApplication.java

--- src/main/java/com/virtualpairprogrammers/FleetmanRegistryApplication.java
+++ src/main/java/com/virtualpairprogrammers/FleetmanRegistryApplication.java
@@ -2,7 +2,13 @@ package com.virtualpairprogrammers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import com.netflix.appinfo.AmazonInfo;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -11,9 +17,12 @@ public class FleetmanRegistryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FleetmanRegistryApplication.class, args);
 	}
-	
+
+	// <<Added for New Course
+	// Problem - this data changes after the EIP has been allocated, it seems 
+	// this stops correct functioning. 
 	@Bean
-	public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils utils)  {
+	public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils utils) {
 		final EurekaInstanceConfigBean instance = new EurekaInstanceConfigBean(utils);
 //		{
 //			@Scheduled(initialDelay = 30000L, fixedRate = 30000L)
@@ -35,6 +44,7 @@ public class FleetmanRegistryApplication {
 		instance.setNonSecurePort(8010);
 
 		return instance;
-	}	
-	
+	}
+
+
 }
