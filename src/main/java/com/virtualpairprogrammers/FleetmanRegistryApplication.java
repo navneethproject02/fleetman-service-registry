7478f76cac2a1c193c7d7b27b131b7c6cba93124
VPPCourseUser <contact@capabilitytm.com>
2017-02-10 16:49:20 +0000

    Update FleetmanRegistryApplication.java

--- src/main/java/com/virtualpairprogrammers/FleetmanRegistryApplication.java
+++ src/main/java/com/virtualpairprogrammers/FleetmanRegistryApplication.java
@@ -11,4 +11,30 @@ public class FleetmanRegistryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FleetmanRegistryApplication.class, args);
 	}
+	
+	@Bean
+	public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils utils)  {
+		final EurekaInstanceConfigBean instance = new EurekaInstanceConfigBean(utils);
+//		{
+//			@Scheduled(initialDelay = 30000L, fixedRate = 30000L)
+//			public void refreshInfo() {
+//				AmazonInfo newInfo = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+//				if (!this.getDataCenterInfo().equals(newInfo)) {
+//					((AmazonInfo) this.getDataCenterInfo()).setMetadata(newInfo.getMetadata());
+//					this.setHostname(newInfo.get(AmazonInfo.MetaDataKey.publicHostname));
+//					this.setIpAddress(newInfo.get(AmazonInfo.MetaDataKey.publicIpv4));
+//					this.setDataCenterInfo(newInfo);
+//					this.setNonSecurePort(8010);
+//				}
+//			}         
+//		};
+		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+		instance.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
+		instance.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
+		instance.setDataCenterInfo(info);
+		instance.setNonSecurePort(8010);
+
+		return instance;
+	}	
+	
 }
