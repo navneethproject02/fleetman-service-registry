4fc790a22a04a8c0a953126488cf0954c2003785
navneethproject02 <navneethproject02@gmail.com>
2017-04-11 14:49:16 -0400

    Update FleetmanRegistryApplication.java

--- src/main/java/com/virtualpairprogrammers/FleetmanRegistryApplication.java
+++ src/main/java/com/virtualpairprogrammers/FleetmanRegistryApplication.java
@@ -2,14 +2,7 @@ package com.virtualpairprogrammers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.commons.util.InetUtils;
-import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.beans.factory.annotation.Value;
-
-import com.netflix.appinfo.AmazonInfo;
 
 @SpringBootApplication
 @EnableEurekaServer
