ea103d787499e0a33685770111fbdf06a7328b3e
navneethproject02 <navneethproject02@gmail.com>
2017-04-11 14:48:49 -0400

    Update FleetmanRegistryApplication.java

--- src/main/java/com/virtualpairprogrammers/FleetmanRegistryApplication.java
+++ src/main/java/com/virtualpairprogrammers/FleetmanRegistryApplication.java
@@ -15,43 +15,7 @@ import com.netflix.appinfo.AmazonInfo;
 @EnableEurekaServer
 public class FleetmanRegistryApplication {
 
-        @Value("${server.port}")
-        private int port;
-
-	public static void main(String[] args) {
+      public static void main(String[] args) {
 		SpringApplication.run(FleetmanRegistryApplication.class, args);
 	}
-
-	// <<Added for New Course
-	// Problem - this data changes after the EIP has been allocated, it seems 
-	// this stops correct functioning. 
-	@Bean
-	public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils utils) {
-
-// The following is needed if running on Spring Cloud prior to the "Dalston" release train.
-// See the VirtualPairprogrammer.com Video for details
-		final EurekaInstanceConfigBean instance = new EurekaInstanceConfigBean(utils)
-		{
-			@Scheduled(initialDelay = 30000L, fixedRate = 30000L)
-			public void refreshInfo() {
-				AmazonInfo newInfo = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
-				if (!this.getDataCenterInfo().equals(newInfo)) {
-					((AmazonInfo) this.getDataCenterInfo()).setMetadata(newInfo.getMetadata());
-					this.setHostname(newInfo.get(AmazonInfo.MetaDataKey.publicHostname));
-					this.setIpAddress(newInfo.get(AmazonInfo.MetaDataKey.publicIpv4));
-					this.setDataCenterInfo(newInfo);
-					this.setNonSecurePort(port);
-				}
-			}         
-		};
-		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
-		instance.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
-		instance.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
-		instance.setDataCenterInfo(info);
-		instance.setNonSecurePort(port);
-
-		return instance;
-	}
-
-
 }
